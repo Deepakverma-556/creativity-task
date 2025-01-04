3c0223dc44c591aa22bda6f3a0c3ace2925bb1fb
Deepak Verma <deepakradialcode@gmail.com>
2025-01-04 15:56:38 +0530

    add min-h-screen

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -4,7 +4,7 @@ import moon from "../assets/images/moon.png"
 
 const Moon = () => {
     return (
-        <div className='md:min-h-screen flex items-center justify-center overflow-hidden bg-black'>
+        <div className='min-h-screen flex items-center justify-center overflow-hidden bg-black'>
             <div className='text-[10px] w-[640px] max-md:h-[498px] h-[640px] relative'>
                 <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-md:w-28 max-md:h-28 rounded-full bg-gradient-to-br h-44 w-44 from-[#FA8607] via-[#FCB033] to-[#FA8607] shadow-[0px_0px_22px_2px] shadow-yellow-400'></div>
                 <div className='earth absolute border-solid border-white border rounded-full top-14 max-lg:top-28 left-10 max-lg:left-[20%] w-[480px] max-lg:w-96 max-md:w-64 max-md:h-64 max-lg:h-96 h-[480px] max-md:left-[30%] before:absolute max-sm:left-[16%] max-sm:top-[25%]'>
